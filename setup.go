53ed04458ca523c7a65fe12b3532aa2ae795c3f5
agan83 <agan83@163.com>
2023-12-03 08:20:17 +0800

    developing

--- setup.go
+++ setup.go
@@ -31,6 +31,22 @@ func SetupEchoGroup(group *echo.Group) *echo.Group {
 	return getDefaultServer().SetupEchoGroup(group)
 }
 
+func MakeJwtHandler(secret string) echo.MiddlewareFunc {
+	return getDefaultServer().MakeJwtHandler(secret)
+}
+
+func MakeLoginHandler() echo.MiddlewareFunc {
+	return getDefaultServer().MakeAuditLogHandler()
+}
+
+func MakeAuditLogHandler() echo.MiddlewareFunc {
+	return getDefaultServer().MakeAuditLogHandler()
+}
+
+func HandleAuthenticate(c echo.Context) error {
+	return getDefaultServer().HandleAuthenticate(c)
+}
+
 func getDefaultServer() *IAMServer {
 	return defaultServer
 }
@@ -43,14 +59,22 @@ func CreateUser(schema, name string, alias string, password string) (*User, erro
 	return getDefaultServer().CreateUser(schema, name, alias, password)
 }
 
-func CreateDefaultUser(db *gorm.DB, schema, name string, alias string, password string) (*User, error) {
+func CreateDefaultUser(schema, name string, alias string, password string) (*User, error) {
 	return getDefaultServer().CreateDefaultUser(schema, name, alias, password)
 }
 
-func CreateRole(db *gorm.DB, schema string, name string, alias string, patterns []string) (*Role, error) {
+func CreateRole(schema string, name string, alias string, patterns []string) (*Role, error) {
 	return getDefaultServer().CreateRole(schema, name, alias, patterns)
 }
 
-func CreateDefaultRole(db *gorm.DB, schema string, name string, alias string, patterns []string) (*Role, error) {
+func CreateDefaultRole(schema string, name string, alias string, patterns []string) (*Role, error) {
 	return getDefaultServer().CreateDefaultRole(schema, name, alias, patterns)
+}
+
+func AddRoleForUser(domain string, user string, role string) (bool, error) {
+	return getDefaultServer().AddRoleForUser(domain, user, role)
+}
+
+func AddRolesForUser(domain string, user string, roles []string) (bool, error) {
+	return getDefaultServer().AddRolesForUser(domain, user, roles)
 }
